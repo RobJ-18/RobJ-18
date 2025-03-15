059ba0ff31a077b3e0af91bf4e68ce11cca01ae5
RobJ-18 <robert.james397@gmail.com>
2025-03-15 01:03:32 +0000

    Create stop VM Proxmox.sh

---
+++ stop VM Proxmox.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Directories to search
+dir1="/etc/pve/lxc"
+dir2="/etc/pve/qemu-server"
+
+# Array to hold .conf file names (without extension)
+files=()
+
+echo
+
+# Find .conf files in the first directory
+if [ -d "$dir1" ]; then
+    for file in "$dir1"/*.conf; do
+        if [ -e "$file" ]; then
+            filename=$(basename "$file" .conf)
+            files+=("$filename")
+            echo "$(( ${#files[@]} )). $filename"
+        fi
+    done
+else
+    echo "No directory found!"
+fi
+
+# Find .conf files in the second directory
+if [ -d "$dir2" ]; then
+    for file in "$dir2"/*.conf; do
+        if [ -e "$file" ]; then
+            filename=$(basename "$file" .conf)
+            files+=("$filename")
+            echo "$(( ${#files[@]} )). $filename"
+        fi
+    done
+else
+    echo "No directory found!"
+fi
+
+# Check if any .conf files were found
+if [ "${#files[@]}" -eq 0 ]; then
+    echo "No VMs found"
+    exit 1
+fi
+
+# Prompt user to select a file
+while true; do
+    echo
+    read -p "Please select a VM from the list to stop: " selection
+
+    # Validate user input
+    if [[ "$selection" =~ ^[0-9]+$ ]] && [ "$selection" -ge 1 ] && [ "$selection" -le "${#files[@]}" ]; then
+        selected_file="${files[$((selection-1))]}"
+        echo "VM $selected_file will be stopped"
+        # Run the command `qm start 'file name'`
+        qm stop "$selected_file"
+        break
+    else
+        echo "VM does not exist"
+    fi
+done
